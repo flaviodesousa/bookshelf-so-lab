7e748a06552a1ed5b40f6b62ddab9581151d5197
Flavio de Sousa <fl@viodesousa.com>
2016-07-25 23:22:24 +0200

    bookshelf-js-eager-loading-third-table

---
+++ 38493811.js
@@ -0,0 +1,32 @@
+var knex = require('knex')({client: 'sqlite3',connection: {filename: 'data.sqlite3'}, debug: true});
+var bookshelf = require('bookshelf')(knex);
+Promise = require('bluebird');
+
+Promise.all([
+  function() {
+    return knex.schema.createTableIfNotExists('customers', function(t) {
+      t.increments('id').primary();
+      t.string('emailAddress').notNullable().unique();
+      t.timestamps(true, true);
+    });
+  },
+  function() {
+    return knex.schema.createTableIfNotExists('contacts', function(t) {
+      t.increments('id').primary();
+      t.string('value').notNullable();
+      t.integer('contact_type_id').references('contact_types.id');
+      t.string('contactable_type').notNullable();
+      t.integer('contactable_id').notNullable();
+    });
+  },
+  function() {
+    knex.schema.createTableIfNotExists('contact_types', function(t) {
+      t.increments('id').primary();
+      t.string('value').notNullable();
+    });
+  },
+  ]).then(function() {
+    console.log('done');
+  }).catch(function(e) {
+    console.error(e);
+  })
