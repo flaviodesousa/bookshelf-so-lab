7fd354e292b3b961b7e8233fc41c007a1c2b0ad5
Flávio de Sousa <flavio.desousa@rohde-schwarz.com>
2017-05-08 15:49:22 +0200

    Playing with attach()

---
+++ 43832862.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: { filename: 'data.sqlite3' },
+    debug: true
+});
+const bookshelf = require('bookshelf')(knex);
+const bcrypt = require('bcrypt-then');
+bookshelf.plugin('registry');
+//--------------------------------------------------------
+Promise.all([
+  knex.schema.createTable('user', table => {
+    table.increments('id');
+    table.string('username');
+    table.string('password');
+    table.unique('username');
+  }),
+  knex.schema.createTable('identity', table => {
+    table.increments('id');
+    table.string('type');
+    table.string('value');
+  }),
+  knex.schema.createTable('identity_user', table => {
+    table.integer('user_id').references('user.id');
+    table.integer('identity_id').references('identity.id');
+    table.unique(['user_id', 'identity_id']);
+  })
+]);
+//--------------------------------------------------------------------------------
+
+const Identity = bookshelf.model('Identity', {
+  tableName: 'identity',
+
+  user() {
+    return this.belongsToMany('User');
+  }
+});
+
+const User = bookshelf.model('User', {
+  tableName: 'user',
+
+  initialize() {
+    this.on('saving', model => {
+      if (model.hasChanged('username')) {
+        return model.set('username', String(model.attributes.username).toLowerCase());
+      }
+    });
+    this.on('saving', model => {
+      if (model.hasChanged('password')) {
+        return bcrypt.hash(model.attributes.password, 10)
+          .then(hash => model.set('password', hash));
+      }
+    });
+  },
+
+  identities() {
+    return this.belongsToMany('Identity');
+  }
+});
+//-----------------------------------------------------------
+function create(username, email, password) {
+    return bookshelf.transaction((t) => {
+        return new User({ username, password } )
+          .save(null, { transacting: t })
+          .tap(console.log('----------- user'))
+          .tap(user => console.dir(user))
+          .tap(console.log('----------- user.identities()'))
+          .tap(user => console.dir(user.identities()))
+          .tap(console.log('-----------'))
+          .then(user => new Identity({ type: 'email', value: email })
+            .save(null, { transacting: t })
+            .tap(console.log('----------- identity'))
+            .tap(identity => console.dir(identity))
+            .tap(console.log('-----------'))
+            .then(identity => new User({id: user.id}).identities()
+              .attach(identity.id, { transacting: t })))
+          .then(() => console.log('created'));
+      })
+      .catch(err => {
+        console.log('errr');
+        console.dir(err);
+      });
+}
